251f57a00378640d1b2cad310ddf529444cc5c21
Zhehao Mao <zhehao.mao@gmail.com>
2012-06-29 17:03:26 +0000

    get rid of opentunnel script

--- opentunnel.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Open a local tunnel on the database server. 
-# Forward port 27017 on the remote server to 27018 on localhost.
-# takes the hostname of the remote server as an argument
-
-ssh -fN -L 27018:localhost:27017 $1
