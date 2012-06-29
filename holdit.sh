a027c147caed126730054c3da8654792c4aa1281
Zhehao Mao <zhehao.mao@gmail.com>
2012-06-29 21:18:11 +0000

    add holdit.sh script

---
+++ holdit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ -z $1 ]; then
+    echo "Usage: $0 user@hostname"
+    exit 1
+fi
+
+MONGO_SERVER=$1
+
+source "$(dirname $0)/pbrc.sh"
+bash "$(dirname $0)/startup.sh" $MONGO_SERVER
+
+# cd into the mongo repository
+cd "$MONGO_DIR"
+
+configstr="globalExtraOption = {numThreads: 24, numSeconds: 86400, testServerInfo: $SERVER_INFO}"
+
+mongo localhost:27018 --eval "$configstr" perfbench/findone.js
