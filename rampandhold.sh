897de57e389215ecaa600e8034ed467ce575ba63
Zhehao Mao <zhehao.mao@gmail.com>
2012-06-30 14:40:00 +0000

    clean results database before running test and dump data afterwards

--- rampandhold.sh
+++ rampandhold.sh
@@ -10,12 +10,16 @@ fi
 
 MONGO_SERVER=$1
 
+bash "$(dirname $0)/cleanandrestart.sh"
+
 source "$(dirname $0)/pbrc.sh"
 bash "$(dirname $0)/startup.sh" $MONGO_SERVER
 
 echo "Ramping up..."
 bash "$(dirname $0)/rampup.sh" $MONGO_SERVER
 
+echo "Dumping data"
+mongoexport -d experiment -c results -o testinfo.json
+
 echo "Holding..."
 nohup bash "$(dirname $0)/holdit.sh" $MONGO_SERVER &
-
