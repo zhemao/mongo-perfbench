f2ceea53199fe2d660b679c6e5d87b89a16d68b6
Zhehao Mao <zhehao.mao@gmail.com>
2012-07-02 15:32:01 -0400

    add summary script

---
+++ benchsummary.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import json
+import sys
+
+def summarize_data(data):
+    name = data['name']
+    numThreads = data['numThreads']
+    numTrials = data['numTrials']
+    avg = sum([trial[name] for trial in data['trials']]) / numTrials
+
+    return '%s\t%d\t%f' % (name, numThreads, avg)
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        f = open(sys.argv[1])
+    else:
+        f = sys.stdin
+
+    print 'operation\t# threads\tavg latency'
+
+    for line in f:
+        data = json.loads(line.strip())
+        print summarize_data(data)
