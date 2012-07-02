ca5716522e5afdebdb7f0750212fbba17fd810b6
Zhehao Mao <zhehao.mao@gmail.com>
2012-07-02 09:22:00 -0400

    add runbench.sh script

---
+++ runbench.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+	echo "Must specify hosts file"
+fi
+
+HOSTS_FILE=$1
+
+MONGOD_SERVER=$(head -n 1 $HOSTS_FILE)
+
+tail -n +2 $HOSTS_FILE | while read host; do
+	ssh $host "~/mongo/perfbench/rampandhold.sh $MONGOD_SERVER"
+	scp ${host}:testinfo.json $host-testinfo.json
+	scp ${host}:holdit.pid $host-holdit.pid
+done
+
+tail -n +2 $HOSTS_FILE | while read host; do
+	ssh $host "kill $(cat $host-holdit.pid)" 
+done
