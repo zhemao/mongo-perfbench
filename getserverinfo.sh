990b3f42348373c6d96a38998c13aea35bd7339e
Zhehao Mao <zhehao.mao@gmail.com>
2012-07-02 17:40:19 -0400

    oops, switch away from hard tabs

--- getserverinfo.sh
+++ getserverinfo.sh
@@ -6,16 +6,16 @@ KREL=$(uname -r)
 ARCH=$(uname -p)
 
 if [ $KNAME == 'Linux' ]; then
-	OS=$(uname -o)
-	FS=$(df -T ~/datadb | awk 'NR == 2 {print $2}')
+    OS=$(uname -o)
+    FS=$(df -T ~/datadb | awk 'NR == 2 {print $2}')
 elif [ $KNAME == 'Darwin' ]; then
-	# These are all pretty much assumptions. 
-	# Mac OSX doesn't really provide utilities to query for this info
-	OS='Mac OSX'
-	FS='HFS+' 
+    # These are all pretty much assumptions. 
+    # Mac OSX doesn't really provide utilities to query for this info
+    OS='Mac OSX'
+    FS='HFS+' 
 fi
 
 echo "{hostname: \"localhost:27018\", kernelName: \"$KNAME\", \
-		kernelVersion: \"$KVER\", kernelRelease: \"$KREL\", \
-		Platform: \"$ARCH\", fileSystem: \"$FS\", OS: \"$OS\"}"
-		
+        kernelVersion: \"$KVER\", kernelRelease: \"$KREL\", \
+        Platform: \"$ARCH\", fileSystem: \"$FS\", OS: \"$OS\"}"
+        
--- runbench.sh
+++ runbench.sh
@@ -6,8 +6,8 @@
 # The hosts on the remaining lines are the load testing servers
 
 if [ -z "$1" ]; then
-	echo "Must specify hosts file"
-	exit 1
+    echo "Must specify hosts file"
+    exit 1
 fi
 
 HOSTS_FILE=$1
@@ -17,18 +17,18 @@ MONGOD_SERVER=$(head -n 1 $HOSTS_FILE)
 i=1
 
 tail -n +2 $HOSTS_FILE | while read host; do
-	echo "$host -> $MONGOD_SERVER"
-	ssh -n $host "~/mongo/perfbench/rampup.sh $MONGOD_SERVER"
-	scp ${host}:results.json load$i-results.json
-	ssh -n $host "nohup ~/mongo/perfbench/holdit.sh $MONGOD_SERVER < /dev/null &> ~/holdit.log &" 
-	scp ${host}:holdit.pid $host-holdit.pid
-	i=$(($i+1))
+    echo "$host -> $MONGOD_SERVER"
+    ssh -n $host "~/mongo/perfbench/rampup.sh $MONGOD_SERVER"
+    scp ${host}:results.json load$i-results.json
+    ssh -n $host "nohup ~/mongo/perfbench/holdit.sh $MONGOD_SERVER < /dev/null &> ~/holdit.log &" 
+    scp ${host}:holdit.pid $host-holdit.pid
+    i=$(($i+1))
 done
 
 tail -n +2 $HOSTS_FILE | while read host; do
-	if [ -f $host-holdit.pid ]; then
-		echo "Killing holdit.sh on $host"
-		ssh -n $host "kill $(cat $host-holdit.pid)" 
-		rm $host-holdit.pid
-	fi
+    if [ -f $host-holdit.pid ]; then
+        echo "Killing holdit.sh on $host"
+        ssh -n $host "kill $(cat $host-holdit.pid)" 
+        rm $host-holdit.pid
+    fi
 done
