88cd2e9b9d43cfb5001cb9e0804685b59917fe70
Zhehao Mao <zhehao.mao@gmail.com>
2012-07-05 16:24:43 -0400

    get rid of extra space

--- runbench.sh
+++ runbench.sh
@@ -21,7 +21,7 @@ tail -n +2 $HOSTS_FILE | while read host operation threads; do
     ssh -n $host "~/mongo/perfbench/rampup.sh $MONGOD_SERVER $operation $threads"
     scp ${host}:results.json load$i-results.json
     ssh -n $host "nohup ~/mongo/perfbench/holdit.sh $MONGOD_SERVER $operation $threads \
-                     < /dev/null &> ~/holdit.log &" 
+                    < /dev/null &> ~/holdit.log &" 
     scp ${host}:holdit.pid $host-holdit.pid
     i=$(($i+1))
 done
