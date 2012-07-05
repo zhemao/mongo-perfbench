ec85c128399afad847d26153d9e5fa669a826f5d
Zhehao Mao <zhehao.mao@gmail.com>
2012-07-05 11:01:51 -0400

    export variables separately in pbrc

--- pbrc.sh
+++ pbrc.sh
@@ -3,13 +3,11 @@
 if [ -z "$PBRC" ]; then
     export PBRC=1
 
-    MONGO_DIR="$(dirname $0)/.."
+    export MONGO_DIR="$(dirname $0)/.."
 
     if [ -d $HOME/bin ] && [ -z `echo $PATH | grep $HOME/bin ` ]; then
-        PATH=$HOME/bin:$PATH
+        export PATH=$HOME/bin:$PATH
     fi
 
-    [ -z $MAXTHREADS ] && MAXTHREADS=24
-
-    export MONGO_DIR PATH MAXTHREADS
+    [ -z $MAXTHREADS ] && export MAXTHREADS=24
 fi
