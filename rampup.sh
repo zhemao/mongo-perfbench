73205e85fb4695df27c5daec3637f60878339f0d
Zhehao Mao <zhehao.mao@gmail.com>
2012-07-06 15:36:37 -0400

    pass in suite name to mongo

--- rampup.sh
+++ rampup.sh
@@ -4,8 +4,8 @@
 # The number of threads used on each run iterate from 1 to $MAXTHREADS
 # After finishing the loop, the results will be dumped to ~/results.json
 
-if [ -z $5 ]; then
-    echo "Usage: $0 user@results-server user@mongod-server operation numthreads increment" 
+if [ -z $6 ]; then
+    echo "Usage: $0 user@results-server user@mongod-server operation numthreads increment suite" 
     exit 1
 fi
 
@@ -14,6 +14,7 @@ MONGO_SERVER=$2
 OPERATION=$3
 MAXTHREADS=$4
 INCREMENT=$5
+SUITE=$6
 
 source "$(dirname $0)/pbrc.sh"
 
@@ -25,7 +26,8 @@ SERVER_INFO=$(ssh $MONGO_SERVER '~/mongo/perfbench/getserverinfo.sh')
 for (( i=$INCREMENT; i<=$MAXTHREADS; i+=$INCREMENT )); do
     ssh $MONGO_SERVER '~/mongo/perfbench/cleanandrestart.sh'
     echo "Load testing with $i threads"
-    configstr="globalExtraOption = {numThreads: $i, databaseURL: \"$MONGO_SERVER\", testServerInfo: $SERVER_INFO}"
+    configstr="globalExtraOption = {numThreads: $i, databaseURL: \"$MONGO_SERVER\", \
+                testServerInfo: $SERVER_INFO}; suiteName = \"$SUITE\";"
 
     mongo --eval "$configstr" $RESULTS_SERVER perfbench/$OPERATION.js
 done
