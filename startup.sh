d11f2000af42f1cf7eb43bde361b4c2aa0a8fec3
Zhehao Mao <zhehao@pt1.sl.10gen.cc>
2012-07-05 19:25:00 -0500

    use lsof absolute path to avoid path nonsense

--- startup.sh
+++ startup.sh
@@ -11,11 +11,11 @@ RESULTS_SERVER=$1
 MONGO_SERVER=$2
 
 # start mongod if necessary
-if ! lsof -i :27017 > /dev/null; then
+if ! /usr/sbin/lsof -i :27017 > /dev/null; then
     ssh -fN -L 27017:localhost:27017 $RESULTS_SERVER
 fi
 
 # start ssh tunnel if necessary
-if ! lsof -i :27018 > /dev/null; then
+if ! /usr/sbin/lsof -i :27018 > /dev/null; then
     ssh -fN -L 27018:localhost:27017 $MONGO_SERVER
 fi
