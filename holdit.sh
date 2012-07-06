faea61041b38036b7cc6ce78779275d9a712f0ae
Zhehao Mao <zhehao.mao@gmail.com>
2012-07-06 14:11:46 -0400

    don't use ssh tunnel

--- holdit.sh
+++ holdit.sh
@@ -14,17 +14,16 @@ OPERATION=$3
 NUMTHREADS=$4
 
 source "$(dirname $0)/pbrc.sh"
-bash "$(dirname $0)/startup.sh" $RESULTS_SERVER $MONGO_SERVER
 
 # cd into the mongo repository
 cd "$MONGO_DIR"
 
-configstr="globalExtraOption = {numThreads: $NUMTHREADS, numSeconds: 7200, databaseURL: \"127.0.0.1:27018\"}"
+configstr="globalExtraOption = {numThreads: $NUMTHREADS, numSeconds: 7200, databaseURL: \"$MONGO_SERVER\"}"
 
 echo -e $$ > ~/holdit.pid
 
 echo "Holding at $NUMTHREADS threads"
 
 while true; do
-    mongo --eval "$configstr" perfbench/$OPERATION.js
+    mongo --eval "$configstr" $RESULTS_SERVER perfbench/$OPERATION.js
 done
--- rampup.sh
+++ rampup.sh
@@ -15,7 +15,6 @@ OPERATION=$3
 MAXTHREADS=$4
 
 source "$(dirname $0)/pbrc.sh"
-bash "$(dirname $0)/startup.sh" $RESULTS_SERVER $MONGO_SERVER
 
 # cd into the mongo repository
 cd "$MONGO_DIR"
@@ -25,8 +24,8 @@ SERVER_INFO=$(ssh $MONGO_SERVER '~/mongo/perfbench/getserverinfo.sh')
 for (( i=1; i<=$MAXTHREADS; i++ )); do
     ssh $MONGO_SERVER '~/mongo/perfbench/cleanandrestart.sh'
     echo "Load testing with $i threads"
-    configstr="globalExtraOption = {numThreads: $i, databaseURL: \"127.0.0.1:27018\", testServerInfo: $SERVER_INFO}"
+    configstr="globalExtraOption = {numThreads: $i, databaseURL: \"$MONGO_SERVER\", testServerInfo: $SERVER_INFO}"
 
-    mongo --eval "$configstr" perfbench/$OPERATION.js
+    mongo --eval "$configstr" $RESULTS_SERVER perfbench/$OPERATION.js
 done
 
