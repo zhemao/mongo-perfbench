c8139f7bed0a41a6d2a5fa5ab3c14e162ca4dd0b
Zhehao Mao <zhehao.mao@gmail.com>
2012-07-06 10:13:59 -0400

    add bgnoise.sh to generate background noise on certain servers

---
+++ bgnoise.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# This script starts the benchmarking
+# It takes as an argument a file listing the hosts you want to connect to
+# The host on the first line is the database server
+# The hosts on the remaining lines are the load testing servers
+
+if [ -z "$1" ]; then
+    echo "Must specify configuration file"
+    exit 1
+fi
+
+HOSTS_FILE=$1
+
+RESULTS_SERVER=$(head -n 1 $HOSTS_FILE)
+MONGOD_SERVER=$(head -n 2 $HOSTS_FILE | tail -n 1)
+
+tail -n +3 $HOSTS_FILE | while read host operation threads; do
+    echo "$host -> $MONGOD_SERVER"
+    ssh -n $host "nohup ~/mongo/perfbench/holdit.sh $RESULTS_SERVER $MONGOD_SERVER \
+                    $operation $threads < /dev/null &> ~/holdit.log &" 
+done
+
