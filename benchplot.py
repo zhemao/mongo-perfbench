d50790b6df84e61a04ffdb973bf487205823c041
Zhehao Mao <zhehao.mao@gmail.com>
2012-07-12 11:15:51 -0400

    add comments to benchplot.py

--- benchplot.py
+++ benchplot.py
@@ -5,9 +5,12 @@ import matplotlib.pyplot as plt
 import sys
 
 def total_ops(trial):
+    """Calculate the total number of operations performed in this trial"""
     return sum(trial[op] for op in ['query', 'insert', 'update', 'delete'])
 
 def summarize_data(data):
+    """Calculate and return the number of threads, the average number of
+    operations performed, and the average latency."""
     name = data['name']
     numThreads = data['numThreads']
     numTrials = data['numTrials']
@@ -17,41 +20,40 @@ def summarize_data(data):
                     
     return numThreads, avgops, avglat
 
-    #return '%s\t\t%d\t\t%6d\t\t%6d' % (name, numThreads, avgops, avglat)
-
 def main():
     if len(sys.argv) > 1:
         f = open(sys.argv[1])
     else:
         f = sys.stdin
 
-    #print 'test name\t# threads\tavg operations\tavg latency (us)'
-    
     allThreads = []
     allOps = []
     allLatency = []
 
+    # go through and append the thread #, ops, and latency 
+    # to the proper lists
     for line in f:
         data = json.loads(line.strip())
         threads, ops, lat = summarize_data(data)
         allThreads.append(threads)
         allOps.append(ops)
         allLatency.append(lat)
-        
-        #print summarize_data(data)
     
     plt.figure(1)
     
+    # plot operations vs threads
     plt.subplot(311)
     plt.xlabel('Threads')
     plt.ylabel('Number of Ops/sec')
     plt.plot(allThreads, allOps)
     
+    # plot latency vs threads
     plt.subplot(312)
     plt.xlabel('Threads')
     plt.ylabel('Latency(micros)')
     plt.plot(allThreads, allLatency)
     
+    # plot latency vs operations
     plt.subplot(313)
     plt.ylabel('Latency(micros)')
     plt.xlabel('Ops/sec')
