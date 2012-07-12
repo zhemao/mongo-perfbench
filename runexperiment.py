d9256356186ceabecf743b1d46cf75a6965d5603
Zhehao Mao <zhehao.mao@gmail.com>
2012-07-12 11:19:01 -0400

    add license boilerplate to all python files

--- runexperiment.py
+++ runexperiment.py
@@ -4,6 +4,21 @@ import json
 import subprocess
 from helpers import *
 
+#    Copyright (C) 2012 10gen Inc.
+#
+#    This program is free software: you can redistribute it and/or  modify
+#    it under the terms of the GNU Affero General Public License, version 3,
+#    as published by the Free Software Foundation.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 # Run a single experiment.
 # Usage: runexperiment.py operation suite config
 # Operation is the operation that is to be run.
